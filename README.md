08464b0b7e0be288f483ac410b5a248583672f85
Przemyslaw Furtak <przemyslawf@gmail.com>
2018-05-04 21:51:10 +0200

    Readme + fix webpack configuration

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # snake
 Snake Game
+
+### DEV:
+1. Run `yarn install` to install all dependencies.
+1. Run `yarn start` command in the root directory.
+2. Run `yarn test` to run unit tests.
+
+### PRODUCTION
+1. Run `webpack` command to create `dist/` directory containing the app
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
 
 module.exports = {
+    mode: 'development',
     entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
     output: {
         path: __dirname + '/dist', // Folder to store generated bundle
         filename: 'bundle.js',  // Name of generated bundle after build
-        publicPath: '/' // public URL of the output directory when referenced in a browser
+        publicPath: '' // public URL of the output directory when referenced in a browser
     },
     module: {  // where we defined file patterns and their loaders
         rules: [
